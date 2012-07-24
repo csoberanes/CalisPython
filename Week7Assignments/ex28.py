37fece321c1280cf2adb2d68cd6d67c1b6a51f27
Cali Soberanes <csoberanes@cnm.edu>
2012-07-24 11:44:34 -0600

    week 7 assignments

---
+++ Week7Assignments/ex28.py
@@ -0,0 +1,10 @@
+
+
+
+print "==  is equal to Checks if the value of two operands are equal or not, if yes then condition becomes true."
+print "!= is not equal to Checks if the value of two operands are equal or not, if values are not equal then condition becomes true."
+print "<> is equal - Checks if the value of two operands are equal or not, if values are not equal then condition becomes true."
+print "> Checks if the value of left operand is greater than the value of right operand, if yes then condition becomes true."
+print "< Checks if the value of left operand is less than the value of right operand, if yes then condition becomes true."
+print ">= Checks if the value of left operand is greater than or equal to the value of right operand, if yes then condition becomes true."
+print "<= Checks if the value of left operand is less than or equal to the value of right operand, if yes then condition becomes true."
---
+++ Week7Assignments/ex34.py
@@ -0,0 +1,15 @@
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+Animal1 =  animals[0]
+Animal2 =  animals[1]
+Animal3 =  animals[2]
+Animal4 =  animals[3]
+Animal5 =  animals[4]
+Animal6 =  animals[5]
+
+print "the first animal is %s" % Animal1
+print "the second animal is %s" % Animal2
+print "the third animal is %s" % Animal3
+print "the fourth animal is %s" % Animal4
+print "the fifth animal is %s" % Animal5
+print "the sixth animal is %s" % Animal6
